3d32972cdcbc8c8a288fc5ac09cf17f544a953e4
Aghogho <aghogho.akpedeye@gmail.com>
2023-01-23 13:15:02 +0000

    created fragments scaffold and add drawable materials

--- app/src/main/java/com/aghogho/messengerappmuzz/MainActivity.kt
+++ app/src/main/java/com/aghogho/messengerappmuzz/MainActivity.kt
@@ -2,10 +2,41 @@ package com.aghogho.messengerappmuzz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toolbar
+import androidx.viewpager.widget.ViewPager
+import com.aghogho.messengerappmuzz.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(binding.toolbarMain)
+
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = ""
+
+        val tabLayout: TabLayout = findViewById(R.id.tab_layout)
+        val viewPager: ViewPager = findViewById(R.id.view_pager)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //return super.onOptionsItemSelected(item)
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
