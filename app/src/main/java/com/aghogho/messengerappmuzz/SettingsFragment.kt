33d143234f2fba71f54a7bd56588468ccd2aedd8
Aghogho <aghogho.akpedeye@gmail.com>
2023-01-27 01:08:11 +0000

    implemented functionality to upload files to storage in fb db

--- app/src/main/java/com/aghogho/messengerappmuzz/SettingsFragment.kt
+++ app/src/main/java/com/aghogho/messengerappmuzz/SettingsFragment.kt
@@ -1,17 +1,25 @@
 package com.aghogho.messengerappmuzz
 
+import android.app.Activity
+import android.app.ProgressDialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import com.aghogho.messengerappmuzz.modelclass.Users
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.StorageTask
+import com.google.firebase.storage.UploadTask
 import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
 
@@ -22,6 +30,10 @@ class SettingsFragment : Fragment() {
     private var userNameSettingsET: TextView? = null
     private var profileImgContainer: CircleImageView? = null
     private var coverImgContainer: ImageView? = null
+    private var REQUEST_CODE = 450
+    private var imageUri: Uri? = null
+    private var uploadedStorageReference: StorageReference? = null
+    private var checkIfCoverImage: String? = ""
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_settings, container, false)
@@ -31,6 +43,7 @@ class SettingsFragment : Fragment() {
         userNameSettingsET = view.findViewById(R.id.username_settings)
         profileImgContainer = view.findViewById(R.id.profile_image_settings)
         coverImgContainer = view.findViewById(R.id.cover_image_settings)
+        uploadedStorageReference = FirebaseStorage.getInstance().reference.child("User Images")
 
         usersReference!!.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
@@ -51,7 +64,76 @@ class SettingsFragment : Fragment() {
 
         })
 
+        view.findViewById<CircleImageView>(R.id.profile_image_settings).setOnClickListener {
+            pickProfileImage()
+        }
+
+        view.findViewById<ImageView>(R.id.cover_image_settings).setOnClickListener {
+            checkIfCoverImage = "cover"
+            pickProfileImage()
+        }
+
         return view
+
+    }
+
+    private fun pickProfileImage() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(intent, REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK && data!!.data != null) {
+            imageUri = data.data
+            Toast.makeText(context, "Uploading...", Toast.LENGTH_LONG).show()
+            uploadImageToFbDatabase()
+        }
+    }
+
+    private fun uploadImageToFbDatabase() {
+        val progressBar = ProgressDialog(context)
+        progressBar.setMessage("Image is uploading, please wait...")
+        progressBar.show()
+
+        if (imageUri != null) {
+            val uploadFileReff = uploadedStorageReference!!.child(System.currentTimeMillis().toString() + ".jpg")
+
+            val uploadTask: StorageTask<*>
+            uploadTask = uploadFileReff.putFile(imageUri!!)
+            uploadTask.continueWithTask<Uri?>(Continuation <UploadTask.TaskSnapshot, Task<Uri>> { task ->
+                if (!task.isSuccessful) {
+                    task.exception?.let {
+                        throw it
+                    }
+                }
+                return@Continuation uploadFileReff.downloadUrl
+            }).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val downloadUrl = task.result
+                    val url = downloadUrl.toString()
+
+                    if (checkIfCoverImage == "cover") {
+                        val mapCoverImg = HashMap<String, Any>()
+                        mapCoverImg["cover"]
+                        usersReference!!.updateChildren(mapCoverImg)
+                        checkIfCoverImage = ""
+                    } else {
+                        val mapProfileImg = HashMap<String, Any>()
+                        mapProfileImg["profile"]
+                        usersReference!!.updateChildren(mapProfileImg)
+                        checkIfCoverImage = ""
+                    }
+                    progressBar.dismiss()
+                }
+            }
+        }
+    }
+
+    private fun pickCoverImage() {
+
     }
 
 }
